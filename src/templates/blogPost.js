af47ea6e0366768521c72aac9edd00ed04d3ef6a
jeroentiggelman <infos@photographicdesign.be>
2020-06-20 17:13:04 +0200

    Added blog post preview and some styling

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
 import Layout from "../components/layout"
 
 export const data = graphql`
@@ -19,7 +20,8 @@ const blogPost = ({ data }) => {
     <Layout>
       <h1>{data.mdx.frontmatter.title}</h1>
       <p>Written by: {data.mdx.frontmatter.author}</p>
-      <p>{data.mdx.body}</p>
+      <MDXRenderer>{data.mdx.body}</MDXRenderer>
+      <Link to="/">&larr; Back to Home</Link>
     </Layout>
   )
 }
